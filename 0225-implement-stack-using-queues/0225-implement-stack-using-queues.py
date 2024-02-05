12b8e2d6ba240ffce935298bb426e6c488f65530
LEESUNGWOOK <40846999+gbk1414@users.noreply.github.com>
2024-02-05 20:43:34 +0900

    Time: 29 ms (92.31%), Space: 16.8 MB (52.90%) - LeetHub

--- 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
+++ 0225-implement-stack-using-queues/0225-implement-stack-using-queues.py
@@ -5,10 +5,11 @@ class MyStack:
     def __init__(self):
         self.queue1 = Queue()
         self.queue2 = Queue()
-
+    
     def push(self, x: int) -> None:
         self.queue1.put(x)
-
+    
+    #FIFO, queue1과 2가 서로 옮겨져도 그 성질이 유지된다.
     def pop(self) -> int:
         if self.empty():
             print("Stack is empty")
@@ -16,18 +17,17 @@ class MyStack:
 
         while self.queue1.qsize() > 1:
             self.queue2.put(self.queue1.get())
-        
         popval = self.queue1.get()
         
         self.queue1, self.queue2 = self.queue2, self.queue1
 
         return popval
             
-
+    #pop과 top은 FIFO구조에서 뺀 값을 다시 put하는 여부의 차이이다.
     def top(self) -> int:
         topval = self.pop()
-        
         self.queue1.put(topval)
+        
         return topval
 
     def empty(self) -> bool:
